669ce8c76b340a3fdb8ad7efbf98c93f6ec63cc4
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2016-09-07 18:56:36 -0500

    use sql for dump

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Chitrana
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # use raw SQL instead of ruby/active-record code to build the dump file
+    config.active_record.schema_format = :sql
   end
 end
