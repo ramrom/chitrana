412d62b24b852ec1f2bea4f0f6e86d407e188830
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2016-10-02 17:17:27 -0500

    add ref methods to check expiration of cache key

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -12,4 +12,13 @@ class Metric < ActiveRecord::Base
   def self.put_cached_data
     Rails.cache.write "mykey", "mydata", expires_in: 5.seconds
   end
+
+  # so the read_entry method will return nil object if the key expired
+  def self.cache_expires_at(cache_key)
+    Time.at(Rails.cache.send(:read_entry, cache_key, {}).expires_at)
+  end
+
+  def self.cache_key_expired?(cache_key)
+    Rails.cache.send(:read_entry, cache_key, {}).expired?
+  end
 end
