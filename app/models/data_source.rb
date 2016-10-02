6939b5d31f577b9d05372e2de798c3d1e5be3ffc
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2016-10-02 18:01:50 -0500

    add pretty print method to data source

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -3,4 +3,20 @@ class DataSource
     conn = PG::Connection.open(dbname: 'chitrana_development')
     # conn.exec 'select * from blah'
   end
+
+  def self.pp(res)
+    rows_count = res.num_tuples
+    column_names = res.fields
+    col_header = column_names.join(', ')
+
+    puts col_header
+
+    for i in 0..rows_count-1
+      row_hash = res[i]
+      row_arr = []
+      column_names.each { |col| row_arr << row_hash[col] }
+      row = row_arr.join(', ')
+      puts row
+    end
+  end
 end
